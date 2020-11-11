b70639b68debb0e72e34e64531b80504219b4704
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-11-11 15:48:08 -0300

    criação do arquivo interface grafica

---
+++ calculadoraPoupancas/src/br/com/java/JanelaPoupanca.java
@@ -0,0 +1,54 @@
+package br.com.java;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+public class JanelaPoupanca extends JFrame{
+	
+	private JTextField campoJuros;
+	private JTextField campoAnos;
+	private JTextField campoDeposito;
+	private JLabel lbJuros;
+	private JLabel lbAnos;
+	private JLabel lbDeposito;
+	private JLabel lbTotal;
+	private JLabel lbResultado;
+	private JButton ok;
+	
+	public JanelaPoupanca() {
+		
+		super("Poupex");
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		this.setBounds(500, 100, 350, 150);
+		
+		JPanel painel = new JPanel();
+		this.setContentPane(painel);
+		
+		lbJuros = new JLabel ("Juros ao mês %:");
+		campoJuros = new JTextField(15);
+		
+		painel.add(lbJuros);
+		painel.add(campoJuros);
+		lbAnos = new JLabel("Numero de anos:");
+		campoAnos = new JTextField(15);
+		
+		painel.add(lbAnos);
+		painel.add(campoAnos);
+		lbDeposito = new JLabel("Depósito mensal R$:");
+		campoDeposito = new JTextField(15);
+		
+		painel.add(lbDeposito);
+		painel.add(campoDeposito);
+		
+		lbTotal = new JLabel("Total poupado R$:");
+		painel.add(lbResultado);
+		
+		ok = new JButton("OK");
+		painel.add(ok);
+	}
+
+}
